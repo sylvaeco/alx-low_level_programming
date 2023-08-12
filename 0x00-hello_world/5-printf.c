112af8db66d0ff97bdd2eb1dfbb5538baf6d9beb
sylvaeco <sylvanusiyoha3@gmail.com>
2023-08-11 17:42:57 -0700

    this uses the printf function to output the desired text

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry point of the code
+ *
+ * Return: Outputs 0 in this instance (Sucess)
+ *
+ * The printf function prints the required text
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a peice of art,\n");
+	return  (0);
+)
