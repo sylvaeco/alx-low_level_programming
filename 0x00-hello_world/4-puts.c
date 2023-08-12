1b8ceed681d10580fae9d7d2bda6cf3e5d269d22
sylvaeco <sylvanusiyoha3@gmail.com>
2023-08-11 17:22:40 -0700

    This is a c file that uses puts for print out an output

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
-/* This is the entry point of te code
+/**
+ * This is the entry point of te code
+ * 
  * The puts function prints the required text
+ * 
  * the return program exits the program with the value 0 equating to a false value
  */
 
