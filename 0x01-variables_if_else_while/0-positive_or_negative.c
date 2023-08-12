d63086f4ae1c290313371cb4ea071159183f35ac
sylvaeco <sylvanusiyoha3@gmail.com>
2023-08-12 15:28:18 -0700

    This script runs the C code to check for positive ir negative values

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+/* 
+ * main - Entry point 
+ *
+ *Return: This returns 0 ( as a success)
+ *
+ * the main contains (void)
+ * so no keyward args were stated
+ *
+ *
+ * */
+
+int main(void)
+{
+	int n;
+	
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n >0)
+		printf("%d is positive \n", n);
+	else if (n == 0)
+		prrintf("%d is zero\n", n);
+	else 
+		printf("%d is negative\n", n);	
+	return (0);
+}
