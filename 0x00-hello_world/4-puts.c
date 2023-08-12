4d281b593ade60e39793b79aacc1368965177e5b
sylvaeco <sylvanusiyoha3@gmail.com>
2023-08-11 17:26:45 -0700

    This is a c file that uses puts for print out an output

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,13 @@
 #include <stdio.h>
 
 /**
- * This is the entry point of te code
- * 
+ * This is the entry point of te code 
  * The puts function prints the required text
- * 
- * the return program exits the program with the value 0 equating to a false value
+ * exits the program with value 0 equating to a false value
  */
 
 
 int main(void)
-{
-
-	    puts("\"Programming is like building a multilingual puzzle");
-	    return (0);
+{	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
