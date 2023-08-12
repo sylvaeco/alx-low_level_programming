2b387f415296163a82851086662c2dc4e4e652da
sylvaeco <sylvanusiyoha3@gmail.com>
2023-08-11 17:09:19 -0700

    This is a c file that uses puts for print out an output

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/* This is the entry point of te code*/
+int main()
+{
+	    /* The puts function prints the required text*/
+	    puts("Programming is like building a multilingual puzzle\n");
+
+	    /* Exit the program with a value of 0 which equaates to false*/
+	    return 0;
+}
