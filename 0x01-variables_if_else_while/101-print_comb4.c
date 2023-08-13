470740f513ee1bc8450a6126747119782306e872
sylvaeco <sylvanusiyoha3@gmail.com>
2023-08-13 11:53:07 -0700

    this code  prints all possible different combinations of three digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -3,7 +3,7 @@
 /**
 * main - Entry point
 *
-* Description: The main function is the first function that 
+* Description: The main function is the first function that
 * is executed bin the program when the execution is started
 *
 * Return: returns 0 (int value)
@@ -13,17 +13,20 @@ int main(void)
 
 {
 	int x, y, z;
-	for (x = 0; x < 10; x++) 
+
+	for (x = 0; x < 10; x++)
 	{
-		for (y = x + 1; y < 10; y++) {
-			for (z = y + 1; z < 10; z++) {
-				if (x != y && x != z && z != y && x < y && y < z) {
+		for (y = x + 1; y < 10; y++) 
+		{
+			for (z = y + 1; z < 10; z++) 
+			{
+				if (x != y && x != z && z != y && x < y && y < z) 
+				{
 					putchar(x + '0');
 					putchar(y + '0');
 					putchar(z + '0');
-					if (x == 7 && y == 8 && z == 9) {
+					if (x == 7 && y == 8 && z == 9)
 						continue;
-					}
 					putchar(',');
 					putchar(' ');
 				}
