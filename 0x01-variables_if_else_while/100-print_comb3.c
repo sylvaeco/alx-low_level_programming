3d1f68a0e90f57f881d990a22de804cd138f216d
sylvaeco <sylvanusiyoha3@gmail.com>
2023-08-13 09:49:29 -0700

    this code prints all possible different combinations of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,14 +13,14 @@ int main(void)
 {
 	int tens;
 	int units;
-	
+
 	for (tens = 0; tens <= 9; tens++)
 	{
 		for (units = tens + 1; units <= 9; units++)
-			{
-				putchar(tens + '0');
-				putchar(units + '0');
-				if (tens < 8)
+		{
+			putchar(tens + '0');
+			putchar(units + '0');
+			if (tens < 8)
 			{
 				putchar(',');
 				putchar(' ');
