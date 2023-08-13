ea7a9b563323686ce52c723825f6b0e5308e3ef4
sylvaeco <sylvanusiyoha3@gmail.com>
2023-08-13 09:43:39 -0700

    this code prints all possible different combinations of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: this always return 0 for code success
+*
+* The putchar was used instead of printf.
+*
+*/
+
+int main(void)
+{
+	int tens;
+	int units;
+	
+	for (tens = 0; tens <= 9; tens++)
+	{
+		for (units = tens + 1; units <= 9; units++)
+			{
+				putchar(tens + '0');
+				putchar(units + '0');
+				if (tens < 8)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
