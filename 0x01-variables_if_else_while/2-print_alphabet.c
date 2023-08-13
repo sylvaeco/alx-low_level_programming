b096d59f723af2e5245e25448021b296496d532c
sylvaeco <sylvanusiyoha3@gmail.com>
2023-08-13 06:06:30 -0700

    this code prints the alphabet in lowercase, followed by a new line using putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ *
+ *main- Entry point
+ *
+ *return - Always runs when the code succeed.
+ *
+ * putchar was used instead of printf
+ */
+
+
+int main(void) 
+{
+	int i;
+	for (i = 97; i <= 122; i++) 
+	{
+		putchar(i);
+	}
+       	putchar('\n');
+        return 0;
+}
+
