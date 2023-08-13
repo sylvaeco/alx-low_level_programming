09aa99a6c6f5fc3d024f9bb21abb8d0ddc1ee9b1
sylvaeco <sylvanusiyoha3@gmail.com>
2023-08-13 07:26:29 -0700

    this code  prints all single digit numbers of base 10 starting from 0, followed by a new line. the char variable isnt used

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: this always return 0 for code success
+*
+* The putchar was used instead of printf.
+*
+*/
+
+
+int main() 
+{
+	int i;
+	
+	for (i = 0; i < 10; i++)
+		putchar(i + '0');
+	putchar('\n');
+	return (0);
+}
