e0d34c08d6500e1a82a4cfd09445f08653eb8e41
sylvaeco <sylvanusiyoha3@gmail.com>
2023-08-13 06:59:42 -0700

    this code prints the alphabet in lowercase, followed by a new line after excluding e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: this always return 0 for code success
+*
+* The putchar was used instead of printf.
+*
+*/ 
+
+
+
+
+int main()
+{
+	int i;
+
+	for (i = 97; i <= 122, i++)
+		if (i != 101 && i != 113)
+			putchar(i);
+	putchar('\n');
+	return (0);
+}
+
