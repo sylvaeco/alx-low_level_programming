cfc83fef5402b70d70624338d569b6555f512462
sylvaeco <sylvanusiyoha3@gmail.com>
2023-08-13 12:02:07 -0700

    this code prints all possible combinations of two two-digit numbers

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+
+/**
+* main - This is the entry point
+*
+* Description: The main function is the first function executed
+* in the program when the code is ran
+*
+* Return: returns 0 for code success
+*/
+
+int main(void)
+{
+	int a, b, c, j, k, m;
+
+	for (c = 48; c < 58; c++)
+	{
+		for (j = 48; j < 58; j++)
+		{
+			for (k = 48; k < 58; k++)
+			{
+				for (m = 48; m < 58; m++)
+				{
+					a = (c * 10) + j;
+					b = (k * 10) + m;
+					if (b > a)
+					{
+						putchar(c);
+						putchar(j);
+						putchar(' ');
+						putchar(k);
+						putchar(m);
+						if (c == 57 && j == 56 && k == 57 && m == 57)
+							continue;
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
