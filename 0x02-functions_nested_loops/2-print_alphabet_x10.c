67fe1a2992ea16ee9033852f028e565dc09d7776
sylvaeco <sylvanusiyoha3@gmail.com>
2023-08-15 10:35:11 -0700

    This C file checks for lowercase character

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* _islower - checks and prints 1 for lowercase alphabet
+* and 0 for non lowercase
+*
+* Description: this code uses the _putchar function to display/print
+* its contents
+*
+* Return: Always 0 (Success), this will not be invoked here
+*
+*
+*/
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return 1;
+	}
+	    else
+		    return 0;
+}
