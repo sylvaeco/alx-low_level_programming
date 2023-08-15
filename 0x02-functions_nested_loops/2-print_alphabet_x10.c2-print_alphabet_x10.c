1c2c0e05eea6042a004842e4386fc92a00c36ba3
sylvaeco <sylvanusiyoha3@gmail.com>
2023-08-15 11:17:50 -0700

    This C file prints 10 times the alphabet, in lowercase, followed by a new line

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c2-print_alphabet_x10.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+* print_alphabet_x10 - prints the lowercase alphabets
+*
+* Description: this code uses the _putchar function to display/print
+* its contents
+*
+* Return: Always 0 (Success), this will not be invoked here
+*
+*
+*/
+
+void print_alphabet_x10(void) 
+
+{
+	char c;
+	int i;
+	for (i = 0; i < 10; i++) 
+	{
+		c = 'a';
+		while (c <= 'z') 
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+	}
+}
