5bc9aa2ce3c9034aa83845a9775eb9f9878dcf65
sylvaeco <sylvanusiyoha3@gmail.com>
2023-08-15 11:50:09 -0700

    This C file prints 10 times the alphabet, in lowercase, followed by a new line

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -15,10 +15,11 @@ void print_alphabet_x10(void)
 {
 	char c;
 	int i;
-	for (i = 0; i < 10; i++) 
+
+	for (i = 0; i < 10; i++)
 	{
 		c = 'a';
-		while (c <= 'z') 
+		while (c <= 'z')
 		{
 			_putchar(c);
 			c++;
