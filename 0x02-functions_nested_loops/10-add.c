3e47f4a4c95b08bee0d56d5962d573381921e5ce
sylvaeco <sylvanusiyoha3@gmail.com>
2023-08-15 16:13:29 -0700

    This C file dds two integers and returns the result

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* add - This function takes two args and return the sum of them
+*
+*@num_1: This is the first nteger argument
+*
+*@num_2: This is the second integer argument
+*
+* Return:: This returns the sum of the above arguments
+*/
+
+
+
+int add(int num_1, int num_2)
+{
+	return (num_1 + num_2);
+}
