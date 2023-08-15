2ff2e6e3f6f07b5b893a055ae76f7a5d5f4595e8
sylvaeco <sylvanusiyoha3@gmail.com>
2023-08-15 12:27:16 -0700

    This C file checks for alphabetic character.

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* _isalpha - checks and prints 1 for uppercase alphabet
+* and 0 for non lowercase
+*
+* @c: is the int that will be used for argument of the function
+*
+* Description: this code uses the _putchar function to display/print
+* its contents
+*
+* Return: Always 0 (Success), this will not be invoked here
+*
+*/
+
+int _isalpha(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
