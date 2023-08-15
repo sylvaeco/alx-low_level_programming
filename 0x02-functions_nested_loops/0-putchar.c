de9f2070dda241d306ba20c378e7465a3bad2845
sylvaeco <sylvanusiyoha3@gmail.com>
2023-08-15 06:25:28 -0700

    This c file prints _putchar, followed by a new line using the putchar function

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,25 +3,25 @@
 /**
 * main - Entry point
 *
-* Description: this code uses the putchar function to display/print
+* Description: this code uses the _putchar function to display/print
 * its contents
 *
-* Return: This returns 0 ( as a success)
+* Return: Always 0 (Success)
 *
 *
 */
 
+int main(void)
 
-int main(void) 
 {
-	putchar('_');
-	putchar('p');
-	putchar('u');
-	putchar('t');
-	putchar('c');
-	putchar('h');
-	putchar('a');
-	putchar('r');
-	putchar('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
