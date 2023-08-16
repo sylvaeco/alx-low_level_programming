5663e8a00b273c72ca2206e14e31d1fe737ae3c9
sylvaeco <sylvanusiyoha3@gmail.com>
2023-08-15 17:26:11 -0700

    This C file prints all the natural numbers below 10 that are multiples of 3 or 5

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+* main - This is the entry point of the function
+* that prints natural numbers below 1024 
+* that are multiples of 3 or 5
+*
+* Return: This return success enoted by 0
+* if the code runs successfully
+*
+*
+*/
+
+int main(void)
+{
+	int sum = 0;
+
+	for (int i = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			sum += i;
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
