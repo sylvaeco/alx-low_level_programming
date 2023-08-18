82822439df06a4b9683554839f3920c9f70da5b2
sylvaeco <sylvanusiyoha3@gmail.com>
2023-08-18 13:49:18 -0700

    This C file prints a mixture of  Buzz Fizz and numbers

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,29 +8,26 @@
 
 int main(void)
 {
-	int main(void)
-	{
-		int i;
+	int i;
 
-		for (i = 1; i < 100; i++)
-			if (i % 15 == 0)
-			{
-				printf("FizzBuzz ");
-			}
-			else if (i % 5 == 0)
-			{
-				printf("Buzz ");
-			}
-			else if (i % 3 == 0)
-			{
-				printf("Fizz ");
-			}
-			else
-			{
-				printf("%d ", i);
-			}
-		printf("Buzz");
-		printf("\n");
-		return (0);
-	}
+	for (i = 1; i < 100; i++)
+		if (i % 15 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	printf("Buzz");
+	printf("\n");
+	return (0);
 }
