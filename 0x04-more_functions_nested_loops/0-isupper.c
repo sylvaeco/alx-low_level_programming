83bd0c4a5da5aa6c59f49c5cfe682680da10ce85
sylvaeco <sylvanusiyoha3@gmail.com>
2023-08-18 09:36:41 -0700

    Committing the main.h file

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,20 +1,19 @@
-#include "main.c"
+nclude "main.h"
 #include <stdio.h>
 
 /**
- * main - checks the code for uppercase
- *
- * Return: Always 0 for success
+ * _isupper - checks for uppercase character
+ * @c: Variable text input
+ * Return: Always 0 for success.
  */
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
 	{
-		return ('1');
+		return (1);
 	}
 	else
 	{
-		return ('0');
+		return (0);
 	}
-	return (0);
 }
