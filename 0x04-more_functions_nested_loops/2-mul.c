aff1bf8c66d31c3ba49fa00427f05c760993b8e3
sylvaeco <sylvanusiyoha3@gmail.com>
2023-08-18 10:37:56 -0700

    This C File multiplies two integers.

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * mul - This takes two integer inputs and multiples themm
+ * to generate an output 
+ *
+ * @a: This is the first integer input
+ *
+ * @b: This is the second integer input
+ *
+ * Return: Always 0 for a success
+ */
+
+
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
