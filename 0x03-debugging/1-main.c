b06ce057cbf4ef45260fe5dc5fa9ff12ded17ba8
sylvaeco <sylvanusiyoha3@gmail.com>
2023-08-18 11:53:10 -0700

    Avoiding the the output to go into an infinite loop

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
-
 /**
- * * main - causes an infinite loop
- * * Return: 0
- * */
-
+* main - causes an infinite loop
+* Return: 0
+*/
 int main(void)
 {
 	int i;
@@ -13,12 +11,13 @@ int main(void)
 
 	i = 0;
 
-	/*while (i < 10)
-	  {
-	  putchar(i);
-	  }*/
-
+	/*
+	* while (i < 10) 
+	* {
+	* Infinite loop - No increase of variable
+	* putchar(i);
+	* }
+	*/
 	printf("Infinite loop avoided! \\o/\n");
-
 	return (0);
 }
