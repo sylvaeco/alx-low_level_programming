c23ddf6e922b9f614cf9ffcdbb6372584927561a
sylvaeco <sylvanusiyoha3@gmail.com>
2023-08-18 10:06:39 -0700

    This C file checks for a digit (0 through 9)

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - check the code
+ * @c: This takes an integer value
+ * Return: Always for success..
+ */
+int main(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	return (0);
+}
