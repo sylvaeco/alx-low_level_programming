31ffd95928b22bf180de93344f7956d5517b8093
sylvaeco <sylvanusiyoha3@gmail.com>
2023-08-18 11:11:42 -0700

    this C file prints the numbers, from 0 to 9, followed by a new line

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* print_numbers - This function prints the integers
+* 0 through 9
+*
+* Return: Always returns void.
+*/
+
+void print_numbers(void)
+{
+	char n;
+	
+	for (n = 48; n <= 57; n++)
+		_putchar(n);
+	_putchar('\n');
+}
