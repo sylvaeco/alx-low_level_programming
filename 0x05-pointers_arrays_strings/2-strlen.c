cd5fa011d3c7fc67dd0288537df46fe6593af408
sylvaeco <sylvanusiyoha3@gmail.com>
2023-08-21 12:32:54 -0700

    This C file write a function that returns the length of a string

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,17 +2,16 @@
 #include <stdio.h>
 
 /**
- * _strlen returns the length of a string
+ * _strlen - returns the length of a string
  *
  * @s: This is the variable value to be evaluated
- *
- * Return: Returns 0 for success
  *
  */
 
 int _strlen(char *s)
 {
 	int len;
+
 	len = 0;
 
 	while (*s != '\0')
@@ -20,5 +19,5 @@ int _strlen(char *s)
 		len++;
 		s++;
 	}
-	return (0);
+	return (len);
 }
