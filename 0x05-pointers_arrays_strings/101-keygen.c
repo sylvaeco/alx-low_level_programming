1db6e705e087cb7cfda955699dc33ecbc90e4556
sylvaeco <sylvanusiyoha3@gmail.com>
2023-08-22 09:50:19 -0700

    This C file generates random valid passwords for the program 101-crackme

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -9,24 +9,24 @@
 */
 int main(void)
 {
-	int i, j, k, l;
+	int i, j, k, s;
 	char c[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	char p[58];
 
 	srand(time(NULL));
-	while (l != 2772)
+	while (s != 2772)
 	{
 		j = rand() % 62;
 		p[i] = c[j];
-		l += c[j];
+		s += c[j];
 		i++;
 	}
 	while (c[k])
 	{
-		if (c[k] == (2772 - l))
+		if (c[k] == (2772 - s))
 		{
 			p[i] = c[k];
-			l += c[k];
+			s += c[k];
 			i++;
 			break;
 		}
