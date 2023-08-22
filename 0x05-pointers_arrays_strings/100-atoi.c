c2cc2b006a05ef523644cbda360beba2f7ef107e
sylvaeco <sylvanusiyoha3@gmail.com>
2023-08-22 04:35:08 -0700

    This C file write a function that convert a string to an integer.

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * _atoi - This converts a string to an integer
+ *
+ * @s: string to convert
+ *
+ * Return: value of integer
+ */
+
+int _atoi(char *s)
+{
+	int result = 0;
+	int sign = 1;
+	int found_number = 0;		        
+	while (*s != '\0')
+	{
+		if (*s == '-')
+		{
+			sign = -1;
+		}
+		else if (*s == '+')
+		{
+			sign = 1;
+		}
+		else if (*s >= '0' && *s <= '9')
+		{
+			found_number = 1;
+			result = result * 10 + (*s - '0');
+		}
+		else if (found_number)
+		{
+			break;
+		}
+		s++;
+	}
+	return (sign * result);
+}
