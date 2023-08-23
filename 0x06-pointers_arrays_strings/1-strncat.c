cc40c7b34269c71f4c69cdb0b4340d3441934cfe
sylvaeco <sylvanusiyoha3@gmail.com>
2023-08-23 14:14:53 -0700

    This C file writes  a function that concatenates two strings.

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strcat - This concatenates two strings using at most n bytes from src
+ *
+ * @dest: The destination string.
+ *
+ * @src: The source string to be appended.
+ * 
+ * @n: The maximum number of bytes to use from src.
+ *
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int dest_len = 0;
+	int i = 0;
+
+	char *result = dest;
+
+	while (dest[dest_len] != '\0')
+	{
+		dest_len++;
+	}
+	while (i < n && src[i] != '\0')
+	{
+		dest[dest_len] = src[i];
+		dest_len++;
+		i++;
+	}
+	dest[dest_len] = '\0';
+	return (result);
+}
