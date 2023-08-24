524e2bd472ec3ffa34e808319cce2c96e328d316
sylvaeco <sylvanusiyoha3@gmail.com>
2023-08-23 18:00:30 -0700

    This C file writes a function that copies a string.

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -5,6 +5,7 @@
  * @src: The string to be appended
  * @n: the maximum number of bytes to use for src
  *
+ * Return: This returns a pointer to the resultant copied string (dest)
  */
 
 char *_strncpy(char *dest, char *src, int n)
