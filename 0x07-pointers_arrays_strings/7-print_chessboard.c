f44b7564607de77a4b1748a22e5157955511984b
sylvaeco <sylvanusiyoha3@gmail.com>
2023-08-28 17:17:03 -0700

    This C file writes a function that prints the chessboard.

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_chessboard - This print chessboard output
+ * @a: pointer to a string
+ * Return: void
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int c , b;
+
+	for (c = 0; c < 8; c++)
+	{
+		for (b = 0; b < 8; b++)
+		{
+			_putchar(a[c][b]);
+		}
+		_putchar('\n');
+	}
+}
