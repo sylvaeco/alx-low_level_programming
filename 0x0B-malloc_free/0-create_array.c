20798a824ac5a8dead607d9e7f5684eb1dc6654a
sylvaeco <sylvanusiyoha3@gmail.com>
2023-09-05 11:57:02 -0700

    This C file writes a function that creates an array of chars, and initializes it with a specific char.

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,18 +9,18 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	char *cr;
+	char *str;
 	unsigned int i;
 
 	if (size == 0)
 		return (NULL);
 
-	cr = malloc(sizeof(c) * size);
-	if (cr == NULL)
+	str = malloc(sizeof(c) * size);
+	if (str == NULL)
 		return (NULL);
 
 	for (i = 0; i < size; i++)
-		cr[i] = c;
+		str[i] = c;
 
-	return (cr);
+	return (str);
 }
