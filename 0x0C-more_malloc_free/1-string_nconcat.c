40a779a4daf6770ca4d5abe317fbb372cd3e1485
sylvaeco <sylvanusiyoha3@gmail.com>
2023-09-07 17:29:07 -0700

    This C file writes a function that concatenates two strings.

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -7,52 +7,44 @@
  * @s2: string two
  * @n: The number of bytes
  *
- * Return: return a pointer to the concatenated string
+ * Return: return a pointer to the concatenated strin
  */
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int s1_len = 0, s2_len = 0, result_len = 0;
-	char *result, *p;
+	unsigned int i, j, len1 = 0, len2 = 0;
+	char *sconcat;
 
 	if (s1 == NULL)
+	{
 		s1 = "";
-
+	}
 	if (s2 == NULL)
+	{
 		s2 = "";
+	}
 
-	while (s1[s1_len] != '\0')
-		s1_len++;
-
-	while (s2[s2_len] != '\0')
-		s2_len++;
-
-	if (n >= s2_len)
-		n = s2_len;
-
-	result_len = s1_len + n;
-	result = (char *)malloc((result_len + 1) * sizeof(char));
-
-	if (result == NULL)
+	while (s1[len1])
+	{
+		len1++;
+	}
+	while (s2[len2] && len2 < n)
+	{
+		len2++;
+	}
+	sconcat = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (sconcat == NULL)
+	{
 		return (NULL);
-
-	p = result;
-
-	while (*s1 != '\0')
-	{
-		*p = *s1;
-		p++;
-		s1++;
 	}
-
-	while (n > 0)
+	for (i = 0; i < len1; i++)
 	{
-		*p = *s2;
-		p++;
-		s2++;
-		n--;
+		sconcat[i] = s1[i];
 	}
-
-	*p = '\0';
-
-	return (result);
+	for (j = 0; j < n && s2[j]; j++)
+	{
+		sconcat[len1 + j] = s2[j];
+	}
+	sconcat[len1 + j] = '\0';
+	return (sconcat);
 }
